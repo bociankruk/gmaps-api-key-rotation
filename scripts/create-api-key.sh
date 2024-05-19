d4dd545742ac4fd6918df8927c8b6d72ea5b8948
Dezydery Czuprynski <dezydery.czuprynski@gravity9.com>
2024-05-19 21:43:13 +0200

    Fix create and add deleting old keys

--- scripts/create-api-key.sh
+++ scripts/create-api-key.sh
@@ -3,14 +3,20 @@
 set -euo pipefail
 
 api_key_name=${API_KEY_NAME:-gha-rotated-key}
-
-gcloud info
+GITHUB_ENV=${GITHUB_ENV:-/tmp/github-output-local}
 
 echo "List existing keys"
 gcloud services api-keys list --format="value(name)"
 
-NEW_KEY=$(gcloud services api-keys create \
+new_key_json=$(gcloud services api-keys create \
     --display-name="${api_key_name}" \
-    --format="value(key)")
+    --format=json \
+    2> /dev/null \
+    | jq -c '.response | {name: .name, api_key: .keyString}')
 
-echo $NEW_KEY
+
+NEW_API_KEY_ID=$(echo $new_key_json | jq -r '.name')
+NEW_API_KEY_SECRET=$(echo $new_key_json | jq -r '.api_key')
+
+echo "NEW_API_KEY_ID=$NEW_API_KEY_ID" >> $GITHUB_ENV
+echo "NEW_API_KEY_SECRET=$NEW_API_KEY_SECRET" >> $GITHUB_ENV
