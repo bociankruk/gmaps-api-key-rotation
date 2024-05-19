984b677014c2a616dc1bb38df585333213696481
Dezydery Czuprynski <dezydery.czuprynski@gravity9.com>
2024-05-19 18:06:57 +0200

    Debug roles

--- scripts/create-api-key.sh
+++ scripts/create-api-key.sh
@@ -2,6 +2,8 @@
 
 api_key_name=${API_KEY_NAME:-gha-rotated-key}
 
+gcloud projects get-iam-policy canvas-rampart-423616-b0
+
 echo "List existing keys"
 gcloud services api-keys list --format="value(name)"
 
